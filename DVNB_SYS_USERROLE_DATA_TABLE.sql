d8fc140bba61e7aca5b7f07ab9661af4f20d4459
Tan Vo <vht1092@gmail.com>
2021-05-18 15:42:07 +0700

    Commit script

---
+++ DVNB_SYS_USERROLE_DATA_TABLE.sql
@@ -0,0 +1,16 @@
+REM INSERTING into FPT.DVNB_SYS_USERROLE
+SET DEFINE OFF;
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('hanhvtm',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('tanvh1',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('thunn2',1);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('trinhvtm',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('thoaptk',1);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('trangqtt',2);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('huongttt4',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('maiptt2',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('myln',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('hauntn',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('hanhntm1',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('huynhnt1',4);
+Insert into FPT.DVNB_SYS_USERROLE (IDUSER,IDROLE) values ('hanptn1',4);
+commit;
